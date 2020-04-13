931bfc58ad5391fedf903996fb91f0901bc0f81d
Alek Konnov <konnov7@gmail.com>
2020-04-13 21:53:44 +0300

    Task 1 complete

--- Project 2/index.js
+++ Project 2/index.js
@@ -1,9 +1,9 @@
 var money;
 var time;
 
-//money = prompt("Ваш бюджет на месяц?", "Ваш бюджет");
-money = 10000;
-time = prompt("Введите дату в формате YYYY-MM-DD?", "YYYY-MM-DD");
+money = prompt("Ваш бюджет на месяц?", "Ваш бюджет");
+//money = 10000;
+time = prompt("Введите дату в формате YYYY-MM-DD?", "2020-10-25");
 
 /*var i = 0;
 while (i < 10) {
@@ -30,3 +30,18 @@ appData = {
     savings: false
 };
 console.log(appData.money);
+// Вычисляем среднедневной бюджет, исходя из заданного периода
+// Определяем текущую дату
+var d = new Date();
+var dayNow = d.getDate();
+var monthNow = d.getMonth() + 1;
+var yearNow = d.getFullYear();
+//Получаем месяц и год бюджета
+monthAfter = parseInt(time.substr(5, 2));
+yearAfter = parseInt(time.substr(0, 4));
+difMonth = monthAfter - monthNow;
+if (yearAfter != yearNow) {
+    difMonth = difMonth + 12 * (yearAfter - yearNow);
+}
+budgetMonth = money/difMonth;
+alert("Ежемесячный бюджет на " + difMonth + " месяцев  = " + Math.round(budgetMonth));
