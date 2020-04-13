4a891b6a0de4177963fd264f94735f63e64851e8
Alek Konnov <konnov7@gmail.com>
2020-04-13 19:40:27 +0300

    Changes Prj 2

--- Project 2/index.js
+++ Project 2/index.js
@@ -1,8 +1,32 @@
 var money;
 var time;
 
-money = prompt("Ваш бюджет на месяц?", "Ваш бюджет");
-
+//money = prompt("Ваш бюджет на месяц?", "Ваш бюджет");
+money = 10000;
 time = prompt("Введите дату в формате YYYY-MM-DD?", "YYYY-MM-DD");
 
-money;
+/*var i = 0;
+while (i < 10) {
+    console.log("Выводим элемент " + i);
+    i++;
+}
+console.log("Final countdown:");
+for (var num = 100; num > 0 ; num--) {
+console.log(num);
+}*/
+
+var expenses = new Object(money, time);
+expenses = {
+    money: time
+};
+
+var appData = new Object(money, time, expenses);
+appData = {
+    money: money,
+    timeData: time,
+    expenses: expenses,
+    optionalExpenses: "",
+    income: [],
+    savings: false
+};
+console.log(appData.money);
