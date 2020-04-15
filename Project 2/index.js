a454a7436c36ff4f524f19e89cbb2be727e198fb
AlekFletch <darren7@rambler.ru>
2020-04-15 17:44:55 +0300

    Sec new js

--- Project 2/index.js
+++ Project 2/index.js
@@ -1,21 +1,25 @@
 let money = prompt("Ваш бюджет на месяц?", ''),
-time = prompt("Введите дату в формате YYYY-MM-DD", '');
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
-budget: money,
-expenses: {},
-optionalExpences: {},
-income: [],
-timeData: time,
-savings: false
+    budget: money,
+    expenses: {},
+    optionalExpences: {},
+    income: [],
+    timeData: time,
+    savings: false
 };
 
 //appData.expenses.a1 = a2;
 //appData.expenses.a3 = a4;
 
-for (let i = 0; i < 2; i++){
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    b = prompt("Во сколько обойдется?", '');
-    appData.expenses[a] = b;
+for (let i = 0; i < 2; i++) {
+ let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+     b = prompt("Во сколько обойдется?",'');
+
+     appData.expenses[a] = b;
 
 }
+
+
+alert(appData.budget / 30);
