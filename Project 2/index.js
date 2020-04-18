b76a7faadf6ef4cebd71c8f6427541901456609f
AlekFletch <darren7@rambler.ru>
2020-04-18 15:46:27 +0300

    Task 3 0,5done

--- Project 2/index.js
+++ Project 2/index.js
@@ -57,14 +57,48 @@ function chooseExpenses() {
     } while (i < 2);
 }
 chooseExpenses();
-appData.moneyPerDay = (appData.budget / 30).toFixed(2);
-alert("Ваш бюджет на день составляет " + appData.moneyPerDay); 
 
-function checkSavings(){
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed(2);
+    alert("Ваш бюджет на день составляет " + appData.moneyPerDay);
+}
+
+detectDayBudget();
+
+function detectLevel() {
+    if (appData.moneyPerDay <= 100) {
+
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay <= 2000) {
+        console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Высокий уровень достатка");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+detectLevel();
+
+function checkSavings() {
     if (appData.savings == true) {
         let save = +prompt("Какова сумма накоплений?"),
             percent = +prompt("Укажите процент?");
-        appData.monthIncome = save/100/12*percent;    
-        alert("Ежемесячный доход с  ваших накоплений: "+ appData.monthIncome)
+        appData.monthIncome = save / 100 / 12 * percent;
+        alert("Ежемесячный доход с  ваших накоплений: " + appData.monthIncome)
+    }
+}
+checkSavings();
+//Создаем функцию для определения необязательных расходов
+function chooseOptExpenses() {
+    for (let index = 0; index < 3; index++) {
+        let a = prompt("Введите статью необязательных расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+        appData.exoptionalExpences[a] = b;
+        index++;
+    } else {
+        index = index - 1;
+    } 
+        
     }
 }
