2e87dd39cf6dffb1ac8edf48dc8728f2243eaaf2
AlekFletch <darren7@rambler.ru>
2020-04-30 15:55:57 +0300

    Task 7 30 perc done

---
+++ budget-app/src/js/main.js
@@ -0,0 +1,20 @@
+
+let startBut = document.getElementById('start'),
+budgetValue = document.querySelector('.budget-value'),
+dayBudgetValue = document.querySelector('.daybudget-value'),
+levelValue = document.querySelector('.level-value'),
+expensesValue = document.querySelector('.expenses-value'),
+optExpensesValue = document.querySelector('.optionalexpenses-value'),
+incomeValue = document.querySelector('.income-value'),
+mothSavingsValue = document.querySelector('.monthsavings-value'),
+yearSavingsValue = document.querySelector('.yearsavings-value');
+
+let inputFields = document.getElementsByClassName('expenses-item');
+
+let expensesItemBtn = document.getElementsByClassName('expenses-item-btn');
+expensesItemBtn[0].style.background = 'blue';
+
+
+
+//mothSavingsValue.style.background = 'blue';
+
