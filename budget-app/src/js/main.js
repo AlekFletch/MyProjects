341f3af6a400b8ccc529d6281eb1c81c5a18f9d1
AlekFletch <darren7@rambler.ru>
2020-05-04 12:54:43 +0300

    Task 7 Full Done

--- budget-app/src/js/main.js
+++ budget-app/src/js/main.js
@@ -1,21 +1,44 @@
 
-let startBut = document.getElementById('start'),
-budgetValue = document.querySelector('.budget-value'),
-dayBudgetValue = document.querySelector('.daybudget-value'),
-levelValue = document.querySelector('.level-value'),
-expensesValue = document.querySelector('.expenses-value'),
-optExpensesValue = document.querySelectorAll('.optionalexpenses-value'),
-incomeValue = document.querySelector('.income-value'),
-mothSavingsValue = document.querySelector('.monthsavings-value'),
-yearSavingsValue = document.querySelector('.yearsavings-value');
+let startBtn = document.getElementById('start'),
+budget = document.querySelector('.budget-value'),
+daybudget = document.querySelector('.daybudget-value'),
+level = document.querySelector('.level-value'),
+expenses = document.querySelectorAll('.expenses-value'),
+optionalexpenses = document.querySelectorAll('.optionalexpenses-value'),
+income = document.querySelector('.income-value'),
+monthsavings = document.querySelector('.monthsavings-value'),
+yearsavings = document.querySelector('.yearsavings-value'),
+choose = document.querySelector('.choose-income'),
+checkbox = document.querySelector('#savings'),
+sum = document.querySelector('#sum'),
+percent = document.querySelector('#percent'),
+year = document.querySelector('.year-value'),
+month = document.querySelector('.month-value'),
+day = document.querySelector('.day-value');
 
 let inputFields = document.getElementsByClassName('expenses-item');
 inputFields[1].nodeValue = 1000;
 //Получаем кнопки "рассчитать" и "утвердить"
 let expensesBtn = document.getElementsByTagName('button');
-let countBtn = expensesBtn[2];
+let count = expensesBtn[2];
 let confirmtBtn = expensesBtn[1];
 
+console.log(startBtn);
+console.log(budget);
+console.log(daybudget);
+console.log(level);
+console.log(expenses);
+console.log(optionalexpenses);
+console.log(income);
+console.log(monthsavings);
+console.log(checkbox);
+console.log(sum);
+console.log(percent);
+console.log(yearsavings);
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(choose);
 
-//mothSavingsValue.style.background = 'blue';
+console.log(count);
 
