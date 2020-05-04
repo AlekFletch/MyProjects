3f8d6e2259adc3d6c5b722e6c527a9d958bb0a60
AlekFletch <darren7@rambler.ru>
2020-05-04 12:22:44 +0300

    Task 7 done 50 pers

--- budget-app/src/js/main.js
+++ budget-app/src/js/main.js
@@ -4,16 +4,17 @@ budgetValue = document.querySelector('.budget-value'),
 dayBudgetValue = document.querySelector('.daybudget-value'),
 levelValue = document.querySelector('.level-value'),
 expensesValue = document.querySelector('.expenses-value'),
-optExpensesValue = document.querySelector('.optionalexpenses-value'),
+optExpensesValue = document.querySelectorAll('.optionalexpenses-value'),
 incomeValue = document.querySelector('.income-value'),
 mothSavingsValue = document.querySelector('.monthsavings-value'),
 yearSavingsValue = document.querySelector('.yearsavings-value');
 
 let inputFields = document.getElementsByClassName('expenses-item');
-
-let expensesItemBtn = document.getElementsByClassName('expenses-item-btn');
-expensesItemBtn[0].style.background = 'blue';
-
+inputFields[1].nodeValue = 1000;
+//Получаем кнопки "рассчитать" и "утвердить"
+let expensesBtn = document.getElementsByTagName('button');
+let countBtn = expensesBtn[2];
+let confirmtBtn = expensesBtn[1];
 
 
 //mothSavingsValue.style.background = 'blue';
