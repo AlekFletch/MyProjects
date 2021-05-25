f259c40ae97cae1a444440b8870e58def0f6312d
Fletch <rusgadgets@gmail.com>
2021-05-25 15:21:05 +0300

    les_38_250521

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -1,16 +1,38 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const tabs = document.querySelectorAll('.tabheader_item'),
-        tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsparent = document.querySelector('.tabheader_items');
+  const tabs = document.querySelectorAll('.tabheader_item'),
+    tabsContent = document.querySelectorAll('.tabcontent'),
+    tabsParent = document.querySelector('.tabheader__items');
 
-    function hideTabContent() {
-       tabsContent.forEach (item => {
-          item.getElementsByClassName.display = 'none';
-       });
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
 
-       tabs.forEach (tab => {
-     tab.classList.remove('tabheader__item_active');
-       });
-    }  
-      
+    tabs.forEach(tab => {
+      tab.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(i = 0) {
+
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader_item_active');
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', function(event) {
+		const target = event.target;
+		if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+		}
+	});
 });
