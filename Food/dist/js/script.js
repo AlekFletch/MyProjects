10ec483afd008cdcbff007a6c5ff6db246350c85
Fletch <rusgadgets@gmail.com>
2021-06-11 14:41:27 +0300

    les_53_110621

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -225,9 +225,9 @@ window.addEventListener('DOMContentLoaded', () => {
     failure: 'Что-то пошло не так...'
   };
 
-forms.forEach(item => {
-  postData(item);
-});
+  forms.forEach(item => {
+    postData(item);
+  });
 
   function postData(form) {
     form.addEventListener('submit', (e) => {
@@ -238,15 +238,27 @@ forms.forEach(item => {
       form.append(statusMessage);
 
       const request = new XMLHttpRequest();
-      request.open('POST','server.php');
-     // request.setRequestHeader('Content-type', 'multipart/form-data');
+      request.open('POST', 'server.php');
+      request.setRequestHeader('Content-type', 'application/json');
       const formData = new FormData(form);
-      request.send(formData);
+      
+      const object = {};
+      formData.forEach(function(value, key){
+        object[key] = value;
+      });
+      
+      const json = JSON.stringify(object);
+      
+      request.send(json);
 
       request.addEventListener('load', () => {
         if (request.status === 200) {
           console.log(request.response);
           statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
         } else {
           statusMessage.textContent = message.failure;
         }
