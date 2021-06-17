a549939c362ab01d3fceac2b0f8db8d4ec7c6e12
Fletch <rusgadgets@gmail.com>
2021-06-17 17:42:21 +0300

    les_56_170621_2

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -258,8 +258,8 @@ window.addEventListener('DOMContentLoaded', () => {
           'Content-type': 'application/json'
         },
         body: JSON.stringify(object)
-      }).then(data => data.text()
-      ).then(data => {
+      }).then(data => data.text())
+      .then(data => {
         console.log(data);
         showThanksModal(message.success);
         form.reset();
