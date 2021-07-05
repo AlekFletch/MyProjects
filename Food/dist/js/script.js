85ad80b4d6d0fdceeca54a3488ff11fce7c1b30a
Fletch <rusgadgets@gmail.com>
2021-07-05 17:18:16 +0300

    les_62_050721

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -320,6 +320,14 @@ window.addEventListener('DOMContentLoaded', function () {
   let slideIndex = 1;
   let offset = 0;
 
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
+  } else {
+    total.textContent = slides.length;
+    current.textContent = slideIndex;
+  }
+
   slidesField.style.width = 100 * slides.length + '%';
   slidesField.style.display = 'flex';
   slidesField.style.transition = '0.5s all';
@@ -338,20 +346,21 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
-
-  });
-
-  next.addEventListener('click', () => {
-    if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
-      offset = 0;
+    
+    if (slideIndex == slides.length) {
+      slideIndex = 1;
     } else {
-      offset += +width.slice(0, width.length - 2);
+      slideIndex++;
     }
-
-    slidesField.style.transform = `translateX(-${offset}px)`;
-
+   
+    if (slideIndex < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
   });
 
+  
   prev.addEventListener('click', () => {
     if (offset == 0) {
       offset = +width.slice(0, width.length - 2) * (slides.length - 1);
@@ -360,6 +369,18 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
+   
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    if (slideIndex < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
 
   });
 
