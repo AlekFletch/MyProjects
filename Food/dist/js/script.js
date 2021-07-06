467650c61cd37ee9292059cd9fcff7f94777b23d
Fletch <rusgadgets@gmail.com>
2021-07-06 15:29:16 +0300

    les_63_060721

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -310,6 +310,7 @@ window.addEventListener('DOMContentLoaded', function () {
     console.log('Нажат левый слайдер!');
   });*/
   const slides = document.querySelectorAll('.offer__slide'),
+    slider = document.querySelector('.offer__slider'),
     prev = document.querySelector('.offer__slider-prev'),
     next = document.querySelector('.offer__slider-next'),
     total = document.querySelector('#total'),
@@ -338,6 +339,64 @@ window.addEventListener('DOMContentLoaded', function () {
     slide.style.width = width;
   });
 
+  slider.style.position = 'relative';
+
+  const indicators = document.createElement('ol'),
+    dots = [];
+  indicators.classList.add('carousel-indicators');
+  indicators.style.cssText = `
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 15;
+  display: flex;
+  justify-content: center;
+  margin-right: 15%;
+  margin-left: 15%;
+  list-style: none;
+  `;
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+
+    dot.style.cssText = `
+    box-sizing: content-box;
+    flex: 0 1 auto;
+    width: 30px;
+    height: 6px;
+    margin-right: 3px;
+    margin-left: 3px;
+    cursor: pointer;
+    background-color: #fff;
+    background-clip: padding-box;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    opacity: .5;
+    transition: opacity .6s ease;
+    `;
+
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+
+    indicators.append(dot);
+    dots.push(dot);
+  }
+
+  function currentInserts() {
+    if (slideIndex < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+
+    dots.forEach(dot => dot.style.opacity = '0.5');
+    dots[(slideIndex - 1)].style.opacity = '1';
+  }
+
   next.addEventListener('click', () => {
     if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
       offset = 0;
@@ -346,21 +405,17 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
-    
+
     if (slideIndex == slides.length) {
       slideIndex = 1;
     } else {
       slideIndex++;
     }
-   
-    if (slideIndex < 10) {
-      current.textContent = `0${slideIndex}`;
-    } else {
-      current.textContent = slideIndex;
-    }
+    currentInserts();
+  
   });
 
-  
+
   prev.addEventListener('click', () => {
     if (offset == 0) {
       offset = +width.slice(0, width.length - 2) * (slides.length - 1);
@@ -369,66 +424,29 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
-   
+
     if (slideIndex == 1) {
       slideIndex = slides.length;
     } else {
       slideIndex--;
     }
 
-    if (slideIndex < 10) {
-      current.textContent = `0${slideIndex}`;
-    } else {
-      current.textContent = slideIndex;
-    }
+    currentInserts();
+
 
   });
 
-  /*
-  showSlides(slideIndex);
+  dots.forEach(dot => {
+    dot.addEventListener('click', (e) => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+      slideIndex = slideTo;
 
-  if (slides.length < 10) {
-    total.textContent = `0${slides.length}`;
-  } else {
-    total.textContent = slides.length;
-  }
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      slidesField.style.transform = `translateX(-${offset}px)`;
 
-  function showSlides(n) {
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
+      currentInserts();
 
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-
-    slides.forEach(item => {
-      item.classList.add('hide', 'fade');
-      item.classList.remove('show');
     });
-    slides[slideIndex - 1].classList.remove('hide');
-
-    slides[slideIndex - 1].classList.add('show');
-
-    // Меняем индикатор "Текущий"
-    if (slideIndex < 10) {
-      current.textContent = `0${slideIndex}`;
-    } else {
-      current.textContent = slideIndex;
-    }
-  }
-
-
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-
-  prev.addEventListener('click', () => {
-    plusSlides(-1);
   });
-
-  next.addEventListener('click', () => {
-    plusSlides(1);
-  });*/
 
 });
