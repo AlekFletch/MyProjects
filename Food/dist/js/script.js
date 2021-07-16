b5a8f80ddee8fa0c8366f0db624813ac7fd2706c
Fletch <rusgadgets@gmail.com>
2021-07-16 11:49:21 +0300

    les_71_160721

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -1,34 +1,19 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-  
+   const tabs = require('./modules/tabs'),
+      calc = require('./modules/calc'),
+      cards = require('./modules/cards'),
+      forms = require('./modules/forms'),
+      modal = require('./modules/modal'),
+      slider = require('./modules/slider'),
+      timer = require('./modules/timer');
 
-  
+   calc();
+   cards();
+   forms();
+   modal();
+   slider();
+   tabs();
+   timer();
 
-  
-
-
-  //Работаем со слайдером
-  /*const images = ['<img src="img/slider/pepper.jpg" alt="pepper"></img>', 
-  '<img src="img/slider/food-12.jpg" alt="food">', '<img src="img/slider/olive-oil.jpg" alt="oil">',
-   '<img src="img/slider/paprika.jpg" alt="paprika">'];
-  const sliderNext = document.querySelector('.offer__slider-next'),
-    sliderPrev = document.querySelector('.offer__slider-prev'),
-    offerSlide = document.querySelector('.offer__slide');
-  sliderNext.addEventListener('click', (e) => {
-    console.log('Нажат правый слайдер!');
-    images.forEach((item, ind) => {
-     console.group(`${item}:${ind}`);
-      if (offerSlide.innerHTML.indexOf(item) > 0) {
-        offerSlide.innerHTML = `
-    ${images[ind + 1]}
-              `;
-      } 
-    });
-    
-  });
-  sliderPrev.addEventListener('click', (e) => {
-    console.log('Нажат левый слайдер!');
-  });*/
- 
-  
 });
